da748aef466cc0cf43819a141e84b08f92422398
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-10-06 17:19:27 +0300

    writing more tests

--- script.test.js
+++ script.test.js
@@ -19,3 +19,19 @@ expect(googleSearch("testtest", dbMock)).toEqual([]);
 expect(googleSearch("dog", dbMock)).toEqual(['dog.com']);
 
 })
+
+it('work with undefined and null input', ()=>{
+    
+ expect(googleSearch(undefined, dbMock)).toEqual([]);
+ expect(googleSearch(null, dbMock)).toEqual([]);
+ 
+ })
+
+
+
+it('does not return more than 3 matches', ()=>{
+    
+ expect(googleSearch('.com', dbMock).length).toEqual(3);
+
+ 
+ })
